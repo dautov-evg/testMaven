798142c556798c1534fc117b9ef9504649cfda3e
Evgeniy Dautov <evg.dautov@mail.ru>
2022-11-27 21:08:28 +0300

    Lesson 038

---
+++ src/main/java/NrtworkUtils.java
@@ -0,0 +1,12 @@
+public class NrtworkUtils {
+    public static void getConnection() {
+        //получаем соединение с сервером
+        try {
+            Thread.sleep(0);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        return;
+
+    }
+}
