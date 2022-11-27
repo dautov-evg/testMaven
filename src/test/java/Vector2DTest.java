a6c48fa7f157959617ebf5b21f2b3080922e1ee0
Evgeniy Dautov <evg.dautov@mail.ru>
2022-11-27 22:03:53 +0300

    Lesson 039 all

--- src/test/java/Vector2DTest.java
+++ src/test/java/Vector2DTest.java
@@ -1,25 +1,37 @@
 import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class Vector2DTest {
+    private final double EPS = 1e-9;
+    private static Vector2D v1;
+
+    //@Before
+    @BeforeClass
+    public static void createNewVector() {
+        v1 = new Vector2D();
+    }
 
     @Test
     public void newVectorShouldHaveZeroLenght() {
-        Vector2D v1 = new Vector2D(); // action
-
         //assertion
-        Assert.assertEquals(0,v1.length(), 1e-9);
+        Assert.assertEquals(0,v1.length(), EPS);
     }
 
-    @Test
-    public void newVectorShouldHaveZeroX() {
-        Vector2D v1 = new Vector2D();
-        Assert.assertEquals(0, v1.getX(), 1e-9);
-    }
+//    @Test
+//    public void newVectorShouldHaveZeroX() {
+//        Assert.assertEquals(0, v1.getX(), EPS);
+//    }
+//
+//    @Test
+//    public void newVectorShouldHaveZeroY() {
+//        Assert.assertEquals(0, v1.getY(), EPS);
+//    }
 
     @Test
-    public void newVectorShouldHaveZeroY() {
-        Vector2D v1 = new Vector2D();
-        Assert.assertEquals(0, v1.getY(), 1e-9);
+    public void newVectorShouldHaveZeroXAndZeroY() {
+        Assert.assertEquals(0, v1.getX(), EPS);
+        Assert.assertEquals(0, v1.getY(), EPS);
     }
 }
